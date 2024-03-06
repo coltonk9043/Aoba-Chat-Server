d7bf7b84e28ab196b62ba6638280c97136ddfddc
Colton <coltonk9043@yahoo.ca>
2024-03-06 17:57:13 -0500

    Update server.py
    Wrapped server code in a class. Improved threading to not check for every single socket every single loop iteration. Decreased CPU thousandfold.

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
-import select
-import signal
-import sys
-import socket
 import json
 import logging
+import socket
+import threading
 
 # Consts
 BUFFER_SIZE = 2048
@@ -34,101 +32,114 @@ BLACK = "§0"
 
     if disconnect
     NOTHING!
-	if(message)
+    if(message)
     message: (message)
     to: (null / @user)
 }
-
 """
-# Variables
-clients = {}
-
-# Setup Logging
-logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG, handlers=[logging.FileHandler("debug.log"), logging.StreamHandler()])
 
 def encode(message):
     return (message + "\r\n").encode()
 
-# Defines a function that will be called when CTRL+C is pressed.
-def signal_handler(sig, frame):
-    for client in clients:
-        client.send(encode("DISCONNECT CHAT/1.0"))
-    logging.info('Interrupt received, shutting down ...')
-    sys.exit(0)
+class AobaChatServer(socket.socket):
+    client_names = {}
+    client_sockets = []
 
-# Main Function
-def main():
-    signal.signal(signal.SIGINT, signal_handler)
-    # Binds the server socket to the local ip.
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serverSocket.bind(('0.0.0.0', 80))
-    # Gets the port of the socket.
-    port = serverSocket.getsockname()[1]
-    serverSocket.listen(10)
-    sockets_list = [serverSocket]
-    
-    logging.info("Will wait for client connections at port " + str(port))
-    logging.info("Waiting for incoming client connections...")
+    # Defines a function that will be called when CTRL+C is pressed.
+    def signal_handler(self, sig, frame):
+        for client in self.client_sockets:
+            client.send(encode("DISCONNECT CHAT/1.0"))
+        logging.info('Interrupt received, shutting down ...')
 
-    # Infinite loop to run whilst the application is active.
-    while True:
-        # Gets the sockets of which we are listening to from the select library.
-        read_sockets, write_sockets, error_sockets = select.select(sockets_list, sockets_list, sockets_list)
-        # For every socket that is in the read_sockets array.
-        for readSocket in read_sockets:
-            # If the read socket is the server socket, that means there is an incoming connection request.
-            if readSocket == serverSocket:
-                try:
-                    # Accept the request
-                    client_socket, client_address = serverSocket.accept()
-                    # Fetch the registration request from the client that was connected.
-                    registration = client_socket.recv(BUFFER_SIZE).decode()
-                    jsonResult = json.loads(registration) 
+    def __init__(self):
+        socket.socket.__init__(self)
 
-                    if("action" in jsonResult and "username" in jsonResult):
-                        if(jsonResult["action"] == "connect"):
-                            # If all succeeds, adds it to the socket list.
-                            sockets_list.append(client_socket)
-                            # Adds the client's name to a list.
-                            name = jsonResult["username"]
-                            clients[client_socket] = name
-                            logging.info("Accepted connection from client address: " + client_socket.getsockname())
-                            logging.info("Connection to client established, waiting to receive messages from user '" + name + "'...")
-                            client_socket.send(encode(json.dumps({ 'user': 'SERVER', 'message' : "200 Registration successful"}, ensure_ascii=False)))
-                        else:
-                            client_socket.send(encode(json.dumps({ 'user': 'SERVER','message' : "400 Invalid registration"}, ensure_ascii=False)))
-                            continue
-                except ValueError as valueError:
-                    break
-                except Exception as e:
-                    logging.error("Exception occured in registration: " + registration + "\n\n" + repr(e))
+        # Setup Logging
+        logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG, handlers=[logging.FileHandler("debug.log"), logging.StreamHandler()])
+
+        #To silence- address occupied!!
+        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bind(('0.0.0.0', 80))
+        self.listen(5)
+
+    def run(self):
+        port = self.getsockname()[1]
+        logging.info("Will wait for client connections at port " + str(port))
+        logging.info("Waiting for incoming client connections...")
+        try:
+            self.accept_clients()
+        except Exception as ex:
+            logging.error(repr(ex))
+        finally:
+            logging.info("Shutting down server.")
+            for client in self.client_sockets:
+                client.close()
+            self.close()
+
+    def accept_clients(self):
+        while True:
+            (client_socket, _) = self.accept()
+            
+            # Get the registration from the client socket.
+            registration = client_socket.recv(BUFFER_SIZE).decode()
+            jsonResult = json.loads(registration) 
+
+            # After decoding json, check to see if the action is connect, and then proceed with registration
+            if("action" in jsonResult and "username" in jsonResult):
+                if(jsonResult["action"] == "connect"):
+                    self.client_sockets.append(client_socket) #Adding client to clients list
+                    name = jsonResult["username"]
+                    self.client_names[client_socket] = name
+
+                    logging.info("Accepted connection from client address: " + client_socket.getsockname()[0])
+                    logging.info("Connection to client established, waiting to receive messages from user '" + name + "'...")
+                else:
+                    client_socket.send(encode(json.dumps({ 'user': 'SERVER','message' : "400 Invalid registration"}, ensure_ascii=False)))
             else:
-                try:
-                    # If a message is received from a client.
-                    user = clients[readSocket]
-                    message = readSocket.recv(BUFFER_SIZE).decode()
-                    jsonResult = json.loads(message) 
+                client_socket.send(encode(json.dumps({ 'user': 'SERVER','message' : "400 Invalid registration"}, ensure_ascii=False)))
 
-                    if("action" in jsonResult):
+            #Receiving data from client
+            newThread = threading.Thread(target = self.recieve, args=(client_socket,)) 
+            newThread.start()
 
-                        if(jsonResult["action"] == "disconnect"):
-                            logging.info("Disconnecting " + user + " from the server.")
-                            sockets_list.remove(readSocket)
-                            clients.pop(readSocket)
-                        elif (jsonResult["action"] == "message"):
-                            # If the message is empty, ignore it.
-                            send_message = jsonResult["message"]
-                            logging.info("<" + user + "> " + send_message)
-                            for writeSocket in write_sockets:
-                                sendUser = clients[writeSocket]
-                                writeSocket.send(encode(json.dumps({ 'user': user, 'message' : send_message}, ensure_ascii=False)))
-                except ConnectionResetError as e:
-                    if user is not None:
-                        logging.info("Disconnecting " + user + " from the server.")
-                        sockets_list.remove(readSocket)
-                        clients.pop(readSocket)
-                except Exception as e: 
-                    logging.error("Exception occured: " + repr(e))
-# Runs the main function.
-main()
+    def recieve(self, client):
+        user = self.client_names[client]
+
+        try:
+            while 1:
+                data = client.recv(BUFFER_SIZE)
+                if data == '':
+                    break
+
+                #Message Received
+                jsonResult = json.loads(data.decode()) 
+                if("action" in jsonResult):
+                    if(jsonResult["action"] == "disconnect"):
+                        break
+                    elif (jsonResult["action"] == "message"):
+                        # If the message is empty, ignore it.
+                        send_message = jsonResult["message"]
+                        logging.info("<" + user + "> " + send_message)
+                        if(send_message.startswith("/")):
+                            client.send(encode(json.dumps({ 'user': "SERVER", 'message' : "You cannot send commands in global silly!"}, ensure_ascii=False)))
+                        else:
+                            self.broadcast(user, send_message)
+        except ConnectionResetError as e:
+            if user is not None:
+                logging.info("Disconnecting " + user + " from the server.")
+                self.client_sockets.remove(client)
+                self.client_names.pop(user, 0)
+                client.close()
+            
+    def broadcast(self, user, message):
+        #Sending message to all clients
+        for client in self.client_sockets:
+            client.send(encode(json.dumps({ 'user': user, 'message' : message}, ensure_ascii=False)))
+
+
+def main():
+    server = AobaChatServer()
+    server.run()
+
+if __name__ == "__main__":
+    main()
